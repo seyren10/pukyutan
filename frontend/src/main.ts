31b3f9abef2166e372602f62a02db90bf2fd7d21
seyren10 <gagarinroyvictor@gmail.com>
2026-08-28 09:50:32 +0800

    added vercel analytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,7 @@ import { queryClient } from "./services/tanstack-query/query-client";
 import "./style.css";
 import { createPinia } from "pinia";
 import router from "./router";
+import { inject } from "@vercel/analytics";
 
 const vueQueryOptions: VueQueryPluginOptions = {
   queryClient,
@@ -19,4 +20,5 @@ const app = createApp(App);
 app.use(VueQueryPlugin, vueQueryOptions);
 app.use(router);
 app.use(pinia);
+inject();
 app.mount("#app");
